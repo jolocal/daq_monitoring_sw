42b5586d729ab88ba76d24064d5c3c576c126331
local <local02070@gmail.com>
2024-06-12 10:00:34 +0900

    TaskSchedulerConfig 주석처리

--- src/main/java/com/example/daq_monitoring_sw/tcp/batch/TaskSchedulerConfig.java
+++ src/main/java/com/example/daq_monitoring_sw/tcp/batch/TaskSchedulerConfig.java
@@ -1,18 +1,18 @@
-package com.example.daq_monitoring_sw.tcp.batch;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-
-@Configuration
-public class TaskSchedulerConfig {
-
-    @Bean
-    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
-        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-        taskScheduler.setPoolSize(10);  // 스레드 풀의 크기 설정
-        taskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler-");
-        taskScheduler.initialize();
-        return taskScheduler;
-    }
-}
+//package com.example.daq_monitoring_sw.tcp.batch;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+//
+//@Configuration
+//public class TaskSchedulerConfig {
+//
+//    @Bean
+//    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+//        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+//        taskScheduler.setPoolSize(10);  // 스레드 풀의 크기 설정
+//        taskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler-");
+//        taskScheduler.initialize();
+//        return taskScheduler;
+//    }
+//}
