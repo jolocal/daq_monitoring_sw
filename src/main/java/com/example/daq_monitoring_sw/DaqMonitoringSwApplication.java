b8aef48fe02eb3a46bc17ee619f5d65e994318e2
local <local02070@gmail.com>
2026-06-22 11:20:28 +0900

    new ec2에 배포 / prod.yaml 수정

--- src/main/java/com/example/daq_monitoring_sw/DaqMonitoringSwApplication.java
+++ src/main/java/com/example/daq_monitoring_sw/DaqMonitoringSwApplication.java
@@ -3,6 +3,7 @@ package com.example.daq_monitoring_sw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 앱 진입점
 @SpringBootApplication
 public class DaqMonitoringSwApplication {
 	public static void main(String[] args) {
